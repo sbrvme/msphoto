b61b38f3cc84e9ce78c55c9670b500a6ccadbaf0
sbrvme <atyxyta@gmail.com>
2024-12-09 16:40:24 +0100

    header mobile nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://sbrvme.github.io',
   base: 'msphoto',
+  integrations: [react()],
 })
