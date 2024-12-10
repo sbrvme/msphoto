8fd132132bf9223c4ece3daef5737402cce5d95d
Oleh Sabirov <atyxyta@gmail.com>
2024-12-10 20:59:36 +0100

    nav and gallery description

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://sbrvme.github.io',
   base: 'msphoto',
-  integrations: [react()],
+  // integrations: [react()],
+  integrations: [],
 })
